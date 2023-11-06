eaa80e54c5e22edcac8ca7732fa71b9a65a2ec6e
Lily Perera Capetillo <lilycapetillo86@gmail.com>
2023-11-06 17:20:32 -0300

    Creating the style of buttons components

---
+++ reflex_init/styles/colors.py
@@ -0,0 +1,14 @@
+from enum import Enum
+
+
+class Color(Enum):
+    PRIMARY = '#0A3143'
+    SECONDARY = '#BFD8D2'
+    BACKGROUND = '#EFEFEF'
+    CONTENTS = '#0A3143'
+
+
+class TextColor(Enum):
+    HEADER = '#0A3143'
+    BODY = '#EFEFEF'
+    FOOTER = '#EFEFEF'
